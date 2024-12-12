4b751c64ab024a93a43e778df737d98d85320c42
biebishan <bishan.bie@vcg.com>
2024-12-12 14:02:18 +0800

    配置基础代码

--- src/list/index.vue
+++ src/list/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-console.log('test')
+
 </script>
 
 <template>
-  <div>123</div>
+ <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <style scoped>
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,27 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createRouter } from "vue-router";
 
-import Index from './list/index.vue'
+import Index from "./list/index.vue";
+import Layout from "@/components/layout/index.vue";
 
 const routes = [
-  { path: '/index', component: Index },
-]
+  { path: "/", redirect: "index" },
+  {
+    path: "/index",
+    component: Layout,
+    children: [
+      {
+        // 当 /user/:id/profile 匹配成功
+        // UserProfile 将被渲染到 User 的 <router-view> 内部
+        path: "profile",
+        component: Index,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
