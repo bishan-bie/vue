09396e3f6ef16ca45a477253647df53eb8277f81
biebishan <bishan.bie@vcg.com>
2024-12-13 14:40:38 +0800

    初始化代码

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -5,18 +5,42 @@
     mode="horizontal"
     @select="handleSelect"
   >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-menu-item index="3" >Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item v-for="r in menuList" :key="r.key">{{ r.title }}</el-menu-item>
   </el-menu>
+  <main> <RouterView /></main>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue';
+import request from '@/utils/request'
 
 const activeIndex = ref('1')
-const activeIndex2 = ref('1')
+const menuList = ref([
+  {
+    key: '1',
+    title: '图片模板类'
+  },
+  {
+    key: '2',
+    title: '通用模板类'
+  },
+  {
+    key: '3',
+    title: '组织机构'
+  },
+  {
+    key: '4',
+    title: '定制频道'
+  },
+  {
+    key: '5',
+    title: '组搜索'
+  }
+ ])
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
+onMounted(() => { 
+  console.log('layout mounted')
+})
 </script>
--- src/router.ts
+++ src/router.ts
@@ -4,15 +4,13 @@ import Index from "./list/index.vue";
 import Layout from "@/components/layout/index.vue";
 
 const routes = [
-  { path: "/", redirect: "index" },
+  { path: "/", redirect: "list" },
   {
-    path: "/index",
+    path: "/",
     component: Layout,
     children: [
       {
-        // 当 /user/:id/profile 匹配成功
-        // UserProfile 将被渲染到 User 的 <router-view> 内部
-        path: "profile",
+        path: "/list",
         component: Index,
       },
     ],
